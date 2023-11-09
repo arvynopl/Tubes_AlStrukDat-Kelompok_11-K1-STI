48106e582cc3ec0f7f54cba9b47859e56dfbd47e
arvynopl <arvynopl@gmail.com>
2023-11-10 02:25:58 +0700

    Add drivermap.c, Update map.c & map.h

--- src/ADT/map.h
+++ src/ADT/map.h
@@ -16,6 +16,10 @@ boolean IsEmptyMapAlbum(Penyanyi P);
 boolean IsFullMapLagu(Album A);
 /* Mengirim true jika album Penyanyi P penuh (P.album_length = MaxEl) */
 boolean IsFullMapAlbum(Penyanyi P);
+/* Mengirim panjang isi (Lagu) Album A */
+int LengthMapLagu(Album A);
+/* Mengirim panjang isi (Album) Penyanyi P */
+int LengthMapAlbum(Penyanyi P);
 /* Mengembalikan true jika Key K adalah member dari Album A */
 boolean IsMemberMapLagu(Album A, Key K);
 /* Mengembalikan true jika Key K adalah member dari Penyanyi P */
@@ -31,12 +35,22 @@ Lagu ValueMapLagu(Album A, Key K);
 /* Mengembalikan nilai value Album dengan Key K dari Penyanyi P */
 Album ValueMapAlbum(Penyanyi P, Key K);
 /* Menambahkan array of character (Title) ST sebagai value elemen Album A dengan Key K. */
-void InsertMapLagu(Penyanyi* P, Album* A, Word* ST);
+void InsertMapLagu(Album* A, Lagu val);
 /* Menambahkan array of character (Title) AT sebagai value elemen Penyanyi P dengan Key K. */
-void InsertMapAlbum(ListPenyanyi* LP, Penyanyi *P, Word* AT);
+void InsertMapAlbum(Penyanyi *P, Album val);
 /* Menghapus array of character (Title) ST sebagai value elemen Album A dengan Key K. */
 void DeleteMapLagu(Album *A, Key K);
 /* Menghapus array of character (Title) AT sebagai value elemen Penyanyi P dengan Key K. */
 void DeleteMapAlbum(Penyanyi *P, Key K);
+
+/* ### Operator Output Map ### */
+/* Mencetak nama lagu */
+void PrintLagu(Lagu L);
+/* Mencetak nama album dan daftar lagu yang ada di dalamnya  */
+void PrintAlbum(Album A);
+/* Mencetak nama penyanyi dan daftar album dan lagu yang ada di dalamnya  */
+void PrintPenyanyi(Penyanyi P);
+/* Mencetak daftar nama penyanyi beserta album dan lagu yang ada di dalamnya  */
+void PrintListPenyanyi(ListPenyanyi LP);
 
 #endif
