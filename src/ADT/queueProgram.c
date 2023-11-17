89814fc1c4e2dae3fe42a398a76c14e3931e42a6
nasywaa-naa <143643795+nasywaa-naa@users.noreply.github.com>
2023-11-17 14:47:27 +0700

    function queue swap

--- src/ADT/queueProgram.c
+++ src/ADT/queueProgram.c
@@ -3,8 +3,7 @@
 #include "queue.h"
 #include "structure.h"
 
-queue_song(){
-    ListPenyanyi list;
+void queueSong(ListPenyanyi list, QueueLagu queue){
     Word inputPenyanyi, inputAlbum;
     int inputLagu;
 
@@ -58,11 +57,64 @@ queue_song(){
         printf("Album tidak valid\n"); //***********BELUM TAU OUTPUTNYA APA
     }
 
+    Enqueue(&queue, (list).penyanyi[j].album_penyanyi[k].lagu_album[inputLagu-1]);
     printf("Berhasil menambahkan lagu \"");
     PrintWord((list).penyanyi[j].album_penyanyi[k].lagu_album[inputLagu-1].lagu_nama);
     printf("\" oleh \"");
     PrintWord(inputPenyanyi);
     printf("\" ke queue.\n");
     //***********************************BAKAL JADI NEW LINE
+}
+
+void queuePlaylist(ListPlaylist list, QueueLagu queue){
+    int inputId;
+    printf("Masukkan ID Playlist: ");
+    scanf("%d", inputId);
+
+    for(int i=0; i<(list).(*playlist + inputId); i++){
+        Enqueue(&queue, (list).playlist.playlist_nama.lagu_playlist);
+    }
+
+    printf("Berhasil menambahkan playlist \"");
+    PrintWord((list).(*playlist+inputId).playlist_nama);
+    printf("\" ke queue.\n");
+
+}
+
+queueSwap(QueueLagu queue){
+    int x, y;
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    if(x < (queue.idxTail+1) && y < (queue.idxTail+1)){
+        if(y<x){
+            int tempx = x;
+            x = y;
+            y = tempx;
+        }
+
+        Lagu tempLagux = queue.lagu_queue[x-1];
+        Lagu tempLaguy = queue.lagu_queue[y-1];
+
+        queue.lagu_queue[x-1] = tempLaguy;
+        queue.lagu_queue[y-1] = tempLagux;
+
+        printf("Lagu \"");
+        PrintWord(queue.lagu_queue[x-1].lagu_nama);
+        prinf("\" berhasil ditukar dengan \"");
+        PrintWord(queue.lagu_queue[y-1].lagu_nama);
+        printf("Lagu \"\n");
+    }
+    else{
+        if(x > (queue.idxTail+1)){
+            printf("Lagu dengan urutan ke %d tidak terdapat dalam queue!\n", x);
+        }
+        else if(y > (queue.idxTail+1)){
+            printf("Lagu dengan urutan ke %d tidak terdapat dalam queue!\n", y);
+        }
+        else{
+            printf("Lagu dengan urutan ke %d dan %d tidak terdapat dalam queue!\n", x, y);
+        }
+    }
 }
 
