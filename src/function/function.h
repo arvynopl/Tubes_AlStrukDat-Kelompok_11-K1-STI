b2b1d3569b7f8194130c0a0c563a9f48f5096a60
arvynopl <53984537+arvynopl@users.noreply.github.com>
2023-11-20 00:28:07 +0700

    Update save, load, & start, ADT and structure (#19)
    * Update linkedlist.c & linkedlist.h
    
    * Update mesinkarakter.c & mesinkarakter.h, mesinkata.c & mesinkata.h, structure.h
    
    * Add driverlinkedlist.c, Update linkedlist.c & linkedlist.h
    
    * Add drivermap.c, Update map.c & map.h
    
    * Minor update on structure.h, stack.h & stack.c, queue.c & queue.h, map.c, linkedlist.c & linkedlist.h, Major update on linkedlist.h and linkedlist.c
    
    * Update mesinkarakter and mesinkata
    
    * Update ADT and structure.h, add load.c & start.c
    
    * Update load.c & start.c successfully, update mesinkarakter, listdinamis, map, queue, and stack ADT
    
    * Update save, load, & start, ADT and structure

--- src/function/function.h
+++ src/function/function.h
@@ -1,7 +1,17 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
 #include "../structure.h"
 #include "../ADT/adt.h"
+
+void DisplayAll();
 
 void STARTPROG(FILE* input);
 
 void LOADPROG(FILE* input);
 
+void SAVEPROG();
+
+void LIST();
+
+#endif
