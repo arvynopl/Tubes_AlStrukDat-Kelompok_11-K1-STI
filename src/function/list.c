60b5466072c95ddf14ab8b29feaa4fbb277da484
arvynopl <arvynopl@gmail.com>
2023-11-20 02:39:12 +0700

    Add list command function

---
+++ src/function/list.c
@@ -0,0 +1,72 @@
+#include "function.h"
+
+void LISTDEFAULT(){
+    printf("Daftar Penyanyi :\n");
+    for (int i = 0; i < LP.penyanyi_length; i++){
+        printf("%d. ", i + 1);
+        PrintWord(LP.penyanyi[i].penyanyi_nama);
+        printf("\n");
+    }
+    printf("\n");
+
+    Word ans1;
+    printf("Ingin melihat album yang ada?(Y/N): ");
+    STARTINPUT();
+    ans1 = GetWord();
+    if (ans1.TabWord[0] == 'Y'){
+        printf("Pilih penyanyi untuk melihat album mereka: ");
+        STARTINPUT();
+        ans1 = GetWord();
+        int idpenyanyi = IndexOfPenyanyiStr(LP, ans1);
+        for (int i = 0; i < LP.penyanyi[idpenyanyi].album_length; i++){
+            printf("%d. ", i + 1);
+            PrintWord(LP.penyanyi[idpenyanyi].album_penyanyi[i].album_nama);
+            printf("\n");
+        }
+        printf("\n");
+        
+        Word ans2;
+        printf("Ingin melihat lagu yang ada?(Y/N): ");
+        STARTINPUT();
+        ans2 = GetWord();
+        if (ans2.TabWord[0] == 'Y'){
+            printf("Pilih album untuk melihat lagu yang ada di album: ");
+            STARTINPUT();
+            ans2 = GetWord();
+            int idalbum = IndexOfAlbumStr(LP, ans1, ans2);
+            for (int i = 0; i < LP.penyanyi[idpenyanyi].album_penyanyi[idalbum].lagu_length; i++){
+                printf("%d. ", i + 1);
+                PrintWord(LP.penyanyi[idpenyanyi].album_penyanyi[idalbum].lagu_album[i].lagu_nama);
+                printf("\n");
+            }
+            printf("\n");
+
+        } else if (ans2.TabWord[0] == 'N'){
+            printf("\n");
+
+        } else{
+            printf("Input invalid...\n");
+        }
+
+    } else if (ans1.TabWord[0] == 'N'){
+        printf("\n");
+
+    } else{
+        printf("Input invalid...\n");
+    }
+
+}
+
+void LISTPLAYLIST(){
+    printf("Daftar playlist yang kamu miliki: \n");
+    if (ListPL.playlist_length == 0){
+        printf("Kamu tidak memiliki playlist.\n");
+    } else{
+        for (int i = 0; i < ListPL.playlist_length; i++){
+            printf("%d. ", i + 1);
+            PrintWord(ListPL.playlist[i].playlist_nama);
+            printf("\n");
+        }
+        printf("\n");
+    }
+}
